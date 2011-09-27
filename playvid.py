0ce3decb79aae99964f3a01e722a58f72ca88f31
Dermot Haughey <hderms@gmail.com>
2011-09-27 12:13:03 -0500

    first commit

---
+++ playvid.py
@@ -0,0 +1,57 @@
+import cv
+
+vidFile = cv.CaptureFromFile( '/home/tarpsocks/mice_videos/vid.avi' )
+
+backgroundFile = cv.CaptureFromFile( '/home/tarpsocks/mice_videos/background.avi' )
+nFrames = int(  cv.GetCaptureProperty( vidFile, cv.CV_CAP_PROP_FRAME_COUNT ) )
+fps = cv.GetCaptureProperty( vidFile, cv.CV_CAP_PROP_FPS )
+waitPerFrameInMillisec = int( 1/fps * 1000/1 )
+width = int(cv.GetCaptureProperty(vidFile, cv.CV_CAP_PROP_FRAME_WIDTH))
+height = int(cv.GetCaptureProperty(vidFile, cv.CV_CAP_PROP_FRAME_HEIGHT))
+
+
+background_fps = cv.GetCaptureProperty(vidFile, cv.CV_CAP_PROP_FPS)
+background_WaitPerFrameInMillisec = int(1/fps * 1000/1)
+
+
+print 'Num. Frames = ', nFrames
+print 'Frame Rate = ', fps, ' frames per sec'
+
+
+frameImg = cv.QueryFrame( vidFile )
+background_img = cv.QueryFrame(backgroundFile)
+for x in xrange(3):
+	background_img = cv.QueryFrame(backgroundFile)
+
+blurred_frame = cv.CreateImage(cv.GetSize(frameImg), cv.IPL_DEPTH_8U, 3)
+grayscale_frame = cv.CreateImage(cv.GetSize(blurred_frame), frameImg.depth, 1)
+bitImg = cv.CreateImage(cv.GetSize(frameImg), frameImg.depth, 1)
+differenceImg = cv.CreateImage(cv.GetSize(frameImg), frameImg.depth, 3)
+writtenImg = cv.CreateImage(cv.GetSize(frameImg), frameImg.depth, 3)
+video_writer = cv.CreateVideoWriter("bitImage.avi", cv.CV_FOURCC('I', '4', '2', '0'), fps, cv.GetSize(frameImg), 1)
+video_difference = cv.CreateVideoWriter("difference.avi", cv.CV_FOURCC('I','4','2', '0'), fps, cv.GetSize(frameImg),1)
+for f in xrange( nFrames ):
+  if f == 1000:
+     break
+
+  frameImg = cv.QueryFrame( vidFile )
+  
+  cv.ShowImage( "My Video Window",  frameImg )
+   
+  cv.WaitKey( waitPerFrameInMillisec  )
+   
+  cv.AbsDiff( background_img, frameImg, differenceImg ) 
+  cv.CvtColor(differenceImg,grayscale_frame, cv.CV_BGR2GRAY) 
+  cv.Threshold(grayscale_frame,bitImg,15,255,cv.CV_THRESH_BINARY)
+  cv.Erode(bitImg,bitImg, iterations=2)
+  cv.Dilate(bitImg, bitImg, iterations=1)
+
+  cv.ShowImage("Processed",bitImg)
+  cv.CvtColor(bitImg, writtenImg, cv.CV_GRAY2BGR)
+  cv.WriteFrame(video_writer, writtenImg)
+  cv.WriteFrame(video_difference, differenceImg)
+# When playing is done, delete the window
+#  NOTE: this step is not strictly necessary, 
+#         when the script terminates it will close all windows it owns anyways
+cv.ReleaseVideoWriter(video_writer)
+cv.DestroyWindow( "My Video Window" )
